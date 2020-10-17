1ba7326c322e9db728df3c53b744bfc57361d0cb
dtdl <860006436@qq.com>
2020-10-17 20:38:44 +0800

    initial

---
+++ src/com/dt/cf/model.py
@@ -0,0 +1,45 @@
+'''
+Created on Oct 17, 2020
+
+@author: GoldenSource
+'''
+
+class BattleField(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+        
+
+
+class Card(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+        
+        
+class Fighter(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+        
